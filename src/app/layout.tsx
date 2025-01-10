545ed24a2495803290b1ca1e9f21451130690017
Hassaan Shaikh <hassaansk786@gmail.com>
2025-01-10 19:41:41 +0530

    Hopeful embed change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import Background from "@/components/Background";
 import Layout from "@/components/Layout";
+import Head from "next/head";
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -19,6 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${dmSans.variable}`}>
+      <Head>
+        <meta name="description" content="Greetings! This is Xalkomak's website!"/>
+        <meta property="og:title" content="Xalkomak" />
+        <meta property="og:description" content="Xalkomak's personal website." />
+        <meta property="og:image" content="public/imgs/Xalkobot_Cropped.png" />
+        <meta property="og:url" content="https://xalkomak.vercel.app/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body className="bg-[#181818] text-white">
         <Background />
         <Navbar />
