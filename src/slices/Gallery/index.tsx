3b4f195486f4e64a2753256cc750821f1eb73f5d
Hassaan Shaikh <hassaansk786@gmail.com>
2025-01-12 17:43:48 +0530

    Added body text to Gallery

--- src/slices/Gallery/index.tsx
+++ src/slices/Gallery/index.tsx
@@ -1,6 +1,10 @@
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
-import { PrismicLink, PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import {
+  PrismicLink,
+  PrismicRichText,
+  SliceComponentProps,
+} from "@prismicio/react";
 import clsx from "clsx";
 
 /**
@@ -14,52 +18,43 @@ export type GalleryProps = SliceComponentProps<Content.GallerySlice>;
 const Gallery = ({ slice }: GalleryProps): JSX.Element => {
   return (
     <section
-      className="container mx-auto flex w-full max-w-8xl flex-col items-center justify-start gap-3 px-4 py-16 text-white first:pt-10 md:px-6 md:py-20 lg:py-24"
-      
+      className="max-w-8xl container mx-auto flex w-full flex-col items-center justify-start gap-3 px-4 py-16 text-white first:pt-10 md:px-6 md:py-20 lg:py-24"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div
-        className="showcase__heading mb-8 flex flex-col items-center justify-center text-balance text-center text-6xl font-medium md:flex-row md:text-7xl"
-      >
+      <div className="showcase__heading mb-8 flex flex-col items-center justify-center text-balance text-center text-6xl font-medium md:flex-row md:text-7xl">
         <PrismicRichText field={slice.primary.title} />
-        <div
-        className="mb-4 flex flex-col items-center justify-center text-balance text-center text-xl font-medium md:flex-row md:text-7xl"
-      >
+      </div>
+      <div className="mb-16 mt-4 flex flex-col items-center text-balance text-center text-xl font-medium md:text-xl">
         <PrismicRichText field={slice.primary.body} />
       </div>
-      </div>
-      <div
-        className="grid gap-2 grid-cols-4">
-        {
-          slice.primary.carousel.map((item, index) => (
-            
-            <div key={index}
-              className={clsx(
-                // item.galleryvariation === "Potrait" && "col-span-1 row-span-2 object-cover",
-                // item.galleryvariation === "Landscape" && "col-span-2 row-span-1 object-cover",
-                // item.galleryvariation === "Square" && "col-span-1 row-span-1 object-cover",
-                // item.galleryvariation === "BigSquare" && "col-span-2 row-span-2 object-cover"
-                `col-span-${item.colspan} row-span-${item.rowspan}`, "text-center")}>
-              <PrismicNextImage
-                      className="rounded-md"
-                      quality={100}
-                      field={item.galleryimage}
-                      alt=""
-                      fallback=""
-                      sizes="(max-width: 128px) 50vw, 30vw" 
-                      />
-              <div
-                className="mt-1 mx-4 text-xs">
-
-                <PrismicRichText field={item.caption} />
-              </div>
+      <div className="grid grid-cols-4 gap-2">
+        {slice.primary.carousel.map((item, index) => (
+          <div
+            key={index}
+            className={clsx(
+              // item.galleryvariation === "Potrait" && "col-span-1 row-span-2 object-cover",
+              // item.galleryvariation === "Landscape" && "col-span-2 row-span-1 object-cover",
+              // item.galleryvariation === "Square" && "col-span-1 row-span-1 object-cover",
+              // item.galleryvariation === "BigSquare" && "col-span-2 row-span-2 object-cover"
+              `col-span-${item.colspan} row-span-${item.rowspan}`,
+              "text-center",
+            )}
+          >
+            <PrismicNextImage
+              className="rounded-md"
+              quality={100}
+              field={item.galleryimage}
+              alt=""
+              fallback=""
+              sizes="(max-width: 128px) 50vw, 30vw"
+            />
+            <div className="mx-4 mt-1 text-xs">
+              <PrismicRichText field={item.caption} />
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
-
-
     </section>
   );
 };
