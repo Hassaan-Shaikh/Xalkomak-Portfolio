652f7f8b607a690818075e1773e6527a4a27db56
Hassaan Shaikh <hassaansk786@gmail.com>
2025-07-09 19:16:04 +0530

    Footer and Icon update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,31 +6,32 @@ import Image from "next/image";
 import dawg_a from "../../../public/imgs/dawg_img_b.png";
 import dawg_b from "../../../public/imgs/doc_img_a.png";
 import dawg_c from "../../../public/imgs/snx_img_a.png";
+import footer from "../../../public/imgs/foot.png";
 
 export default function Footer() {
   const [token, setToken] = useState<number>(0);
   const pathName = usePathname();
 
-  useEffect(() => {
-    setToken(Math.floor(Math.random() * 3) + 1);
-  }, [pathName]);
+  // useEffect(() => {
+  //   setToken(Math.floor(Math.random() * 3) + 1);
+  // }, [pathName]);
 
-  const selectedImage =
-    token === 1 ? dawg_a : token === 2 ? dawg_b : dawg_c;
+  // const selectedImage =
+  //   token === 1 ? dawg_a : token === 2 ? dawg_b : dawg_c;
 
   return (
-    <footer className="relative w-full overflow-hidden">
-      <div className="absolute inset-0">
+    <footer className="relative w-full">
+      <div className="inset-0">
         <Image
-          src={dawg_c}
+          src={footer}
           alt="Footer graphic"
           layout="fill" // Makes the image fill the container
           objectFit="cover" // Ensures the image covers the entire width and height
-          objectPosition="center" // Centers the image
+          objectPosition="base" // Centers the image
           priority // Optimizes image loading
         />
       </div>
-      <div className="relative z-10 flex flex-col items-center justify-center text-white py-16">
+      <div className="relative z-10 flex flex-col items-center justify-center  py-16">
         <p>
           Contact Mail:{" "}
           <a href="mailto:hassaansk786@gmail.com" className="underline">
